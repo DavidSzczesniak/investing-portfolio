dd7d9b2974c09c188289694c185a37398227c568
DavidSzczesniak <david.szczesniak86@gmail.com>
2021-07-03 13:18:51 +0100

    Converted component files to JSX

--- src/components/InfoPage.jsx
+++ src/components/InfoPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router';
-import Button from '../components/Button';
+import Button from './Button';
 import '../css/InfoPage.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../components/Button';
+import Button from './Button';
 import '../css/NavBar.css';
 import { useHistory } from 'react-router';
 
