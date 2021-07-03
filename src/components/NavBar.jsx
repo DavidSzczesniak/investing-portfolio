8364ca2f5c53a85b8421f80cfd96322fc833d3c6
DavidSzczesniak <david.szczesniak86@gmail.com>
2021-07-03 17:48:58 +0100

    small tweaks + improvements to navbar
    - moved the search bar to the navbar
    - changed navigation buttons in the navbar to divs and added Portfolio one ahead of time

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,49 @@
+import axios from 'axios';
 import React from 'react';
-import Button from './Button';
-import '../css/NavBar.css';
 import { useHistory } from 'react-router';
+import AsyncSelect from 'react-select/async';
+import { geckoAPI } from '../constants';
+import '../css/NavBar.css';
 
 const UserList = (props) => {
     const history = useHistory();
+
+    async function getAllCoins() {
+        return await axios
+            .get(`${geckoAPI}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100`)
+            .then((res) => {
+                return res.data.map((coin) => {
+                    return { label: `(${coin.symbol.toUpperCase()}) ${coin.name}`, value: coin.id };
+                });
+            });
+    }
+
+    function handleSearch(query) {
+        if (query) {
+            history.replace(`/asset-view?id=${query}`);
+        }
+    }
     return (
         <div>
             <div className="navbar">
-                <span className="site-logo" onClick={() => history.replace('/')}>
-                    Investii
-                </span>
+                <div className="nav-container">
+                    <span className="site-logo" onClick={() => history.replace('/')}>
+                        Investii
+                    </span>
+                    <div className="search-container">
+                        <AsyncSelect
+                            loadOptions={getAllCoins}
+                            defaultOptions
+                            placeholder="Search assets..."
+                            onChange={(e) => handleSearch(e.value)}
+                        />
+                    </div>
+                </div>
                 <div>
-                    <Button label="Watchlist" click={() => history.replace('/user-list')} />
+                    <div className="nav-btn" onClick={() => history.replace('/user-list')}>
+                        Watchlist
+                    </div>
+                    <div className="nav-btn">Portfolio</div>
                 </div>
             </div>
             {props.title && <div className="page-title">{props.title}</div>}
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import '../css/Main.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import AssetInfo from '../components/AssetInfo';
 import NavBar from '../components/NavBar';
-import AsyncSelect from 'react-select/async';
 import { geckoAPI } from '../constants.js';
-import axios from 'axios';
-import { useHistory } from 'react-router';
+import '../css/Main.css';
 
 const Main = () => {
     const [userAssetList, setAssetList] = useState([]);
@@ -22,35 +21,13 @@ const Main = () => {
             });
     }, [refreshed]);
 
-    async function getAllCoins() {
-        return await axios
-            .get(`${geckoAPI}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100`)
-            .then((res) => {
-                return res.data.map((coin) => {
-                    return { label: `(${coin.symbol.toUpperCase()}) ${coin.name}`, value: coin.id };
-                });
-            });
-    }
-
-    function handleSearch(query) {
-        if (query) {
-            history.replace(`/asset-view?id=${query}`);
-        }
-    }
-
     return (
         <>
-            <NavBar title="Home" />
+            <NavBar />
             <div className="search-page">
-                <AsyncSelect
-                    loadOptions={getAllCoins}
-                    defaultOptions
-                    placeholder="Search assets..."
-                    onChange={(e) => handleSearch(e.value)}
-                />
                 {top20.length > 0 && (
                     <>
-                        <h2>Top 20</h2>
+                        <h2>Top 20 by Market Cap</h2>
                         <div>
                             {top20.map((coin, index) => {
                                 return (
