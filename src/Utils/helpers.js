574dd0efde4cec1905dfb84bdfd102b78e0c34b8
DavidSzczesniak <david.szczesniak86@gmail.com>
2021-11-06 23:05:20 +0000

    Improved asset view page layout and added more market stats
    - improved the general layout and its responsiveness
    - added more market stats

--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -5,3 +5,10 @@ export function isPositive(num) {
 export function normalizeNumber(number, decimalPoints = 2) {
     return Number(number.toFixed(decimalPoints)).toLocaleString();
 }
+
+export function compactNumber(number, digits = 4) {
+    return new Intl.NumberFormat('en-GB', {
+        notation: 'compact',
+        minimumSignificantDigits: digits,
+    }).format(number);
+}
--- src/pages/asset-view.js
+++ src/pages/asset-view.js
@@ -4,6 +4,7 @@ import { AssetName, AssetPrice } from '../components/AssetInfo/AssetInfo';
 import { FavouritesButton } from '../components/FavouritesButton/FavouritesButton.jsx';
 import { AssetChart } from '../components/AssetChart/AssetChart.jsx';
 import '../css/asset-view.scss';
+import { compactNumber } from '../Utils/helpers';
 
 export const AssetView = () => {
     const query = new URLSearchParams(useLocation().search);
@@ -16,11 +17,53 @@ export const AssetView = () => {
         symbol: '$',
     };
 
-    const AssetStat = ({ label, text }) => {
+    const marketStats = [
+        {
+            label: 'Market Cap',
+            value: asset.market_cap,
+        },
+        {
+            label: '24h Volume',
+            value: asset.total_volume,
+        },
+        {
+            label: 'All Time High',
+            value: asset.ath,
+        },
+        {
+            label: 'All Time Low',
+            value: asset.atl,
+        },
+        {
+            label: 'Market Rank',
+            value: `#${asset.market_cap_rank}`,
+            formatNumber: false,
+        },
+    ];
+
+    // coingecko doesn't has these props for all coins for whatever reason,
+    // and they're inaccurate, will change API later
+    if (asset.total_supply) {
+        marketStats.push({
+            label: 'Circulating Supply',
+            value: `${compactNumber(asset.total_supply)} ${asset.symbol.toUpperCase()}`,
+            formatNumber: false,
+        });
+    }
+
+    if (asset.max_supply) {
+        marketStats.push({
+            label: 'Max Supply',
+            value: `${compactNumber(asset.max_supply)} ${asset.symbol.toUpperCase()}`,
+            formatNumber: false,
+        });
+    }
+
+    const AssetStat = ({ label, value, formatNumber = true }) => {
         return (
             <div>
                 <h4>{label}</h4>
-                <p>{text}</p>
+                <p>{formatNumber ? `${currency.symbol}${value.toLocaleString()}` : value}</p>
             </div>
         );
     };
@@ -34,23 +77,22 @@ export const AssetView = () => {
                         <AssetPrice asset={asset} currency={currency.symbol} />
                         <FavouritesButton asset={asset} />
                     </div>
-                    <div className="asset-view__stats">
-                        <AssetStat
-                            label="Market Cap"
-                            text={`${currency.symbol}${asset.market_cap.toLocaleString()}`}
-                        />
-                        <AssetStat
-                            label="24 Hour Volume"
-                            text={`${currency.symbol}${asset.total_volume.toLocaleString()}`}
-                        />
-                        {asset.total_supply && (
-                            <AssetStat
-                                label="Circulating Supply"
-                                text={`${asset.total_supply.toLocaleString()}${asset.symbol.toUpperCase()}`}
-                            />
-                        )}
+                    <div className="asset-view__content">
+                        <AssetChart asset={asset} />
+                        <div className="asset-view__stats">
+                            <h3>Market Stats</h3>
+                            {marketStats.map((stat, index) => {
+                                return (
+                                    <AssetStat
+                                        key={index}
+                                        label={stat.label}
+                                        value={stat.value}
+                                        formatNumber={stat.formatNumber}
+                                    />
+                                );
+                            })}
+                        </div>
                     </div>
-                    <AssetChart asset={asset} />
                 </>
             )}
         </>
