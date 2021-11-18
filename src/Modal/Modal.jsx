4b0aeb200732edadcefb850c6af166f3fc07f4a0
DavidSzczesniak <david.szczesniak86@gmail.com>
2021-11-18 15:34:40 +0000

    Removed console.log leftover

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,14 +1,10 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { createPortal } from 'react-dom';
 import { ModalContext } from '../ModalContext';
 import './Modal.scss';
 
 export const Modal = ({ children }) => {
     const { closeModal } = useContext(ModalContext);
-
-    useEffect(() => {
-        console.log('modal renders now');
-    }, []);
 
     return createPortal(
         <>
